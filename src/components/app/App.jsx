9936e5a87f45907164535a04df60995ddccaa5aa
Grigoriy Gavrilov <greeb.spb@gmail.com>
2022-12-28 17:50:53 +0300

    add basic styles

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import AppHeader from '../app-header/app-header';
+
 function App() {
   return (
-    <div>
+    <div className='wrapper'>
+      <AppHeader />
       <h1>React App</h1>
     </div>
   );
