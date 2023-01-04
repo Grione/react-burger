996b30fd211e3015d91f5f9b357cf278db102c3f
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-04 11:15:17 +0300

    add burger card component

---
+++ src/components/burger-ingredients-card/burger-ingredients-card.jsx
@@ -0,0 +1,19 @@
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import BurgerIngredientsCardStyles from './burger-ingredients-card.module.css';
+
+
+function BurgerIngredientsCard(props) {
+  const card = props.card;
+  return (
+    <li className={BurgerIngredientsCardStyles.card}>
+      <span className={`${BurgerIngredientsCardStyles.counter} text text_type_digits-default`}>2</span>
+      <div className='pl-4 pr-4'>
+        <img src={card.image} alt="" />
+      </div>
+      <span className={`${BurgerIngredientsCardStyles.price} mt-1 text text_type_digits-default`}>{card.price} <CurrencyIcon /></span>
+      <h3 className={`mt-1 text text_type_main-default ${BurgerIngredientsCardStyles.name}`}>{card.name}</h3>
+    </li >
+  )
+}
+
+export default BurgerIngredientsCard; 
