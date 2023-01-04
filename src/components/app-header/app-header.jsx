30c78dbaba95dd203348837cc7200755cfa68e45
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-04 11:14:38 +0300

    add header component

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,4 +1,4 @@
-import { Logo, BurgerIcon, ListIcon, ProfileIcon, Box, Typography } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import headerStyles from './app-header.module.css';
 
 function AppHeader() {
