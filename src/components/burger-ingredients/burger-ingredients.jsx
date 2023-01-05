fdd150a75bd3222ecd4ccb3d312bd9fe4982adee
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-05 11:51:23 +0300

    fix tab component

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,20 +1,39 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurgerIngredientsList from '../burger-ingredients-list/burger-ingredients-list';
 import ingredientsStyles from './burger-ingredients.module.css';
 
 function BurgerIngredients(props) {
+  const [current, setCurrent] = React.useState('buns')
+
   const { data } = props;
   const buns = data.filter((el) => el.type === 'bun');
   const mains = data.filter((el) => el.type === 'main');
   const sauces = data.filter((el) => el.type === 'sauce');
+
   return (
     <div className={ingredientsStyles.ingredients}>
       <h1 className='text text_type_main-large pt-10'>Соберите бургер</h1>
       <ul className={`${ingredientsStyles.tabs} pt-5 pb-10`}>
-        <li><Tab>Булки</Tab></li>
-        <li><Tab>Соусы</Tab></li>
-        <li><Tab>Начинки</Tab></li>
+        <li>
+          <Tab
+            value="buns"
+            active={current === 'buns'}
+            onClick={setCurrent}>Булки</Tab>
+        </li>
+        <li>
+          <Tab
+            value="sauces"
+            active={current === 'sauces'}
+            onClick={setCurrent}>Соусы</Tab>
+        </li>
+        <li>
+          <Tab
+            value="mains"
+            active={current === 'mains'}
+            onClick={setCurrent}>Начинки</Tab>
+        </li>
       </ul>
       <div className={ingredientsStyles['list-wrapper']}>
         <BurgerIngredientsList ingredients={buns} title="Булки" />
