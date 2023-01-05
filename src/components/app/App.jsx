670e8d4f58c1f892018cdc0e4a084910db4bf3bc
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-05 11:14:29 +0300

    rewrite to function App component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,25 +5,17 @@ import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import DATA from '../../utils/data';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { DATA };
-  }
+function App() {
 
-
-  render() {
-
-    return (
-      <div className='wrapper'>
-        <AppHeader />
-        <main className='main container'>
-          <BurgerIngredients data={DATA} />
-          <BurgerConstructor />
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div className='wrapper'>
+      <AppHeader />
+      <main className='main container'>
+        <BurgerIngredients data={DATA} />
+        <BurgerConstructor />
+      </main>
+    </div>
+  );
 }
 
 export default App;
