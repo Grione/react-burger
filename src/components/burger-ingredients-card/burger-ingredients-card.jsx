a3cbcdbefb9d3c5c2aaca09f673402855a16a995
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-05 18:34:08 +0300

    fix propTypes

--- src/components/burger-ingredients-card/burger-ingredients-card.jsx
+++ src/components/burger-ingredients-card/burger-ingredients-card.jsx
@@ -19,15 +19,15 @@ function BurgerIngredientsCard(props) {
 
 BurgerIngredientsCard.propTypes = {
   card: PropTypes.shape({
-    _id: PropTypes.string,
-    name: PropTypes.string,
-    type: PropTypes.string,
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
     proteins: PropTypes.number,
     fat: PropTypes.number,
     carbohydrates: PropTypes.number,
     calories: PropTypes.number,
-    price: PropTypes.number,
-    image: PropTypes.string,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
     image_mobile: PropTypes.string,
     image_large: PropTypes.string,
     __v: PropTypes.number
