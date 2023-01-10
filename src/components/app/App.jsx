9a2a79accec9629fba478b975df5cfe7103ec51d
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-10 17:00:24 +0300

    add fetch function

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import DATA from '../../utils/data';
 
+const URL = 'https://norma.nomoreparties.space/api/ingredients';
+
 function App() {
+  const [state, setState] = useState({ data: [], isLoading: true, hasError: false });
+
+  useEffect(() => {
+    const getIngredientData = async () => {
+      setState({ ...state, hasError: false, isLoading: true });
+
+      try {
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        setState({ ...state, data: data.data, isLoading: false });
+      } catch (error) {
+        setState({ ...state, hasError: true, isLoading: false })
+      }
+    }
+
+    getIngredientData();
+
+  }, [])
+  const { data, isLoading, hasError } = state;
 
   return (
     <div className='wrapper'>
       <AppHeader />
       <main className='main container'>
-        <BurgerIngredients data={DATA} />
-        <BurgerConstructor data={DATA} />
+        {isLoading && 'Loading...'}
+        {hasError && 'Erron defined'}
+        {
+          !isLoading && !hasError && (
+            <>
+              <BurgerIngredients data={data} />
+              <BurgerConstructor data={data} />
+            </>
+          )
+
+        }
+
       </main>
     </div>
   );
