f739b8ad88576fb8960e2e87945f49cd860a65c1
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-12 19:54:48 +0300

    refactoring the IngredienDetails

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,12 +1,8 @@
 import ingredientDetailsStyles from './ingredient-details.module.css';
 import { ingredientPropTypes } from '../../utils/propTypes';
 
-function OrderDetails(props) {
+function IngredientDetails(props) {
   const { name, image_large } = props.data;
-  const calories = props.data.calories;
-  const carbohydrates = props.data.carbohydrates;
-  const fat = props.data.fat;
-  const proteins = props.data.proteins;
 
   return (
     <div className={ingredientDetailsStyles.wrapper}>
@@ -17,28 +13,28 @@ function OrderDetails(props) {
       <ul className={`mt-8 ${ingredientDetailsStyles.list}`}>
         <li className={ingredientDetailsStyles.item}>
           <span className="text text_type_main-default text_color_inactive">Калории,ккал</span>
-          <span className="text text_type_digits-default text_color_inactive">{calories}</span>
+          <span className="text text_type_digits-default text_color_inactive">{props.data.calories}</span>
         </li>
         <li className={ingredientDetailsStyles.item}>
           <span className="text text_type_main-default text_color_inactive">Белки, г</span>
-          <span className="text text_type_digits-default text_color_inactive">{proteins}</span>
+          <span className="text text_type_digits-default text_color_inactive">{props.data.proteins}</span>
         </li>
         <li className={ingredientDetailsStyles.item}>
           <span className="text text_type_main-default text_color_inactive">Жиры, г</span>
-          <span className="text text_type_digits-default text_color_inactive">{fat}</span>
+          <span className="text text_type_digits-default text_color_inactive">{props.data.fat}</span>
         </li>
         <li className={ingredientDetailsStyles.item}>
           <span className="text text_type_main-default text_color_inactive">Углеводы, г</span>
-          <span className="text text_type_digits-default text_color_inactive">{carbohydrates}</span>
+          <span className="text text_type_digits-default text_color_inactive">{props.data.carbohydrates}</span>
         </li>
       </ul>
     </div>
   )
 }
 
-OrderDetails.propTypes = {
+IngredientDetails.propTypes = {
   data: ingredientPropTypes.isRequired,
 }
 
 
-export default OrderDetails; 
+export default IngredientDetails; 
