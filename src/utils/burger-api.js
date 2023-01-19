d0cff64a86f4034069d8d863274b6f4e9a7374f6
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-19 18:07:19 +0300

    add post function

--- src/utils/burger-api.js
+++ src/utils/burger-api.js
@@ -1,12 +1,23 @@
 const URL = 'https://norma.nomoreparties.space/api';
 
 
-function getIngredients() {
+export function getIngredients() {
   return fetch(`${URL}/ingredients`)
     .then((res) => {
       return res.ok ? res.json() : res.json().then(err => Promise.reject(err));
     });
 }
 
-export default getIngredients;
-
+export function postOrder(ids) {
+  return fetch(`${URL}/orders`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({
+      "ingredients": ids
+    })
+  }).then((res) => {
+    return res.ok ? res.json() : res.json().then(err => Promise.reject(err))
+  })
+}
