933f55f79be3bb57d32299760e734f122d5174a7
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-19 18:07:52 +0300

    add total price and post data

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,20 +1,36 @@
-import PropTypes from 'prop-types';
-import { ingredientPropTypes } from '../../utils/propTypes';
 import ConstructorStyles from './burger-constructor.module.css';
 import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import { orderData } from '../../utils/order-data';
 import { useMemo, useState, useContext } from 'react';
 import Modal from '../modal/modal';
 import OrderDetails from '../order-details/order-details';
 import { IngredientsContext } from '../../services/ingredientsContext';
+import { postOrder } from '../../utils/burger-api';
 
 function BurgerConstructor() {
+  const [orderData, setOrderData] = useState();
   const [openedModal, setOpenedModal] = useState(false);
   const ingredientsData = useContext(IngredientsContext)
 
   const bun = useMemo(() => ingredientsData.find((el) => el.type === 'bun'), [ingredientsData]);
   const ingredients = useMemo(() => ingredientsData.filter((el) => el.type !== 'bun'), [ingredientsData]);
 
+  const totalPrice = useMemo(() => ingredients.reduce((acc, current) => { return acc + current.price }, 0) + bun.price * 2, [bun, ingredients])
+
+  const ingredientsIds = useMemo(() => {
+    const a = ingredients.map((el) => el._id);
+    a.push(bun._id);
+    return a;
+  }, [ingredients, bun]);
+
+  function sendOrder(ids) {
+    postOrder(ids).then((data) => {
+      setOrderData(data.order.number);
+      setOpenedModal(true);
+    })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
   return (
     <>
       <div className={ConstructorStyles.constructor}>
@@ -56,17 +72,17 @@ function BurgerConstructor() {
         </div>
         <div className={`pt-10 ${ConstructorStyles['constructor-footer']}`}>
           <div className={ConstructorStyles.total}>
-            <span className='text text_type_digits-medium'>610</span>
+            <span className='text text_type_digits-medium'>{totalPrice}</span>
             <CurrencyIcon type="primary" />
           </div>
-          <Button htmlType="button" type="primary" size="large" onClick={() => { setOpenedModal(true) }}>
+          <Button htmlType="button" type="primary" size="large" onClick={() => { sendOrder(ingredientsIds) }}>
             Оформить заказ
           </Button>
         </div>
       </div>
       {openedModal &&
         <Modal close={() => setOpenedModal(false)}>
-          <OrderDetails id={orderData.id} />
+          <OrderDetails id={orderData} />
         </Modal>}
     </>
 
