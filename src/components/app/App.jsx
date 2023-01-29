1c21fd3724086677a15842c0dccd8e52ed29a482
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-01-29 14:03:12 +0300

    change action types names

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { GET_INGREDIENTS_START, GET_INGREDIENTS_SUCCESS, GET_INGREDIENTS_FAILURE } from '../../services/action-types';
+import { GET_INGREDIENTS_REQUEST, GET_INGREDIENTS_SUCCESS, GET_INGREDIENTS_ERROR } from '../../services/action-types';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
@@ -14,13 +14,13 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch({ type: GET_INGREDIENTS_START });
+    dispatch({ type: GET_INGREDIENTS_REQUEST });
     getIngredients()
       .then((data) => {
         dispatch({ type: GET_INGREDIENTS_SUCCESS, payload: data.data })
       })
       .catch((error) => {
-        dispatch({ type: GET_INGREDIENTS_FAILURE })
+        dispatch({ type: GET_INGREDIENTS_ERROR })
       })
   }, [dispatch]);
 
