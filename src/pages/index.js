55554a83dec16636e05fd7f6d1713d3f4e2f745f
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-02-04 21:58:19 +0300

    add export pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,7 @@ import { LoginPage } from "./login";
 import { RegisterPage } from "./register";
 import { ForgotPassword } from "./forgot-password";
 import { ResetPassword } from "./reset-password";
+import { ProfilePage } from "./profile";
+import { IngredientPage } from "./ingredient";
 
-export { HomePage, LoginPage, RegisterPage, ForgotPassword, ResetPassword } 
+export { HomePage, LoginPage, RegisterPage, ForgotPassword, ResetPassword, ProfilePage, IngredientPage } 
