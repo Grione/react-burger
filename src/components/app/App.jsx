99d8429a63bdc0c49e4f02e205313e2f46abeb87
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-02-04 22:08:56 +0300

    add routes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,51 +1,37 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GET_INGREDIENTS_REQUEST, GET_INGREDIENTS_SUCCESS, GET_INGREDIENTS_ERROR } from '../../services/action-types';
 import AppHeader from '../app-header/app-header';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
-import { getIngredients } from '../../utils/burger-api';
+import {
+  HomePage,
+  LoginPage,
+  RegisterPage,
+  ForgotPassword,
+  ResetPassword,
+  ProfilePage,
+  IngredientPage,
+  NotFound404
+} from '../../pages/';
+
 
 function App() {
 
-  const { isLoading, hasError, ingredients } = useSelector(state => state.ingredients);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch({ type: GET_INGREDIENTS_REQUEST });
-    getIngredients()
-      .then((data) => {
-        dispatch({ type: GET_INGREDIENTS_SUCCESS, payload: data.data })
-      })
-      .catch((error) => {
-        dispatch({ type: GET_INGREDIENTS_ERROR })
-      })
-  }, [dispatch]);
-
   return (
-    <div className='wrapper'>
-      <AppHeader />
-      <main className='main container'>
-        {isLoading && 'Loading...'}
-        {hasError && 'Erron defined'}
-        {
-          !isLoading && !hasError && (
-            <>
-              <DndProvider backend={HTML5Backend}>
-                <BurgerIngredients data={ingredients} />
-                <BurgerConstructor />
-              </DndProvider>
-            </>
-
-
-          )
-
-        }
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className='wrapper'>
+        <AppHeader />
+        <main className='main container'>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/ingredient" element={<IngredientPage />} />
+            <Route path="*" element={<NotFound404 />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
