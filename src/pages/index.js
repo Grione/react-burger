51fd4099ea23c8294d5e465cca1c1574bd22d676
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-02-04 17:55:52 +0300

    add index export pages file

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import { HomePage } from "./home";
+import { LoginPage } from "./login";
+import { RegisterPage } from "./register";
+import { ForgotPassword } from "./forgot-password";
+import { ResetPassword } from "./reset-password";
+
+export { HomePage, LoginPage, RegisterPage, ForgotPassword, ResetPassword } 
