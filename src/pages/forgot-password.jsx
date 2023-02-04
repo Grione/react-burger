1d5b7121c4e9996a35bd096f90d9c15b2a2d23fc
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-02-04 17:55:15 +0300

    add password pages

---
+++ src/pages/forgot-password.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import styles from './login.module.css';
+import { Button, EmailInput } from '@ya.praktikum/react-developer-burger-ui-components'
+
+export function ForgotPassword() {
+  const [email, setEmail] = useState('');
+
+  const onChangeEmail = e => {
+    setEmail(e.target.value)
+  }
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className="title text text_type_main-medium mb-6">Восстановление пароля</div>
+          <div className="form">
+            <div className='mb-6'>
+              <EmailInput
+                onChange={onChangeEmail}
+                value={email}
+                placeholder='Укажите Email'
+              />
+            </div>
+            <div className='mb-20' style={{ textAlign: "center" }}>
+              <Button htmlType="submit">Восстановить</Button>
+            </div>
+          </div>
+          <div className="actions">
+            <div style={{ textAlign: 'center' }}>
+              <span className='text_color_inactive text text_type_main-default mr-1'>Вспомнили пароль?</span>
+              <a href="#" className='text text_type_main-default text_color_accent'>Войти</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/reset-password.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import styles from './login.module.css';
+import { Button, PasswordInput, Input } from '@ya.praktikum/react-developer-burger-ui-components'
+
+export function ResetPassword() {
+  const [password, setPassword] = useState('');
+  const [code, setCode] = useState('');
+
+  const onChangePassword = e => {
+    setPassword(e.target.value)
+  }
+
+  const onChangeCode = e => {
+    setCode(e.target.value)
+  }
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className="title text text_type_main-medium mb-6">Восстановление пароля</div>
+          <div className="form">
+            <div className='mb-6'>
+              <PasswordInput
+                onChange={onChangePassword}
+                value={password}
+                placeholder='Введите новый пароль'
+              />
+            </div>
+            <div className='mb-6'>
+              <Input
+                type='text'
+                onChange={onChangeCode}
+                value={code}
+                placeholder='Введите код из письма'
+              />
+            </div>
+            <div className='mb-20' style={{ textAlign: "center" }}>
+              <Button htmlType="submit">Сохранить</Button>
+            </div>
+          </div>
+          <div className="actions">
+            <div style={{ textAlign: 'center' }}>
+              <span className='text_color_inactive text text_type_main-default mr-1'>Вспомнили пароль?</span>
+              <a href="#" className='text text_type_main-default text_color_accent'>Войти</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
