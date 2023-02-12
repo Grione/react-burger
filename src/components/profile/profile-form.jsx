774866a507d2eae60d737697006a0af1276cb4d4
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-02-12 13:49:41 +0300

    add profile components

---
+++ src/components/profile/profile-form.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Input, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { getUser } from '../../services/actions/auth-actions';
+import styles from './profile-form.module.css';
+
+export default function ProfileForm() {
+
+  const { user } = useSelector(state => state.user);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState("");
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser())
+  }, [])
+
+  return (
+    <>
+      <form>
+        <ul>
+          <li className='mb-6'>
+            <Input
+              value={name}
+              onChange={e => setName(e.target.value)}
+              placeholder={'Имя'}
+              type={'text'}
+              icon={'EditIcon'}
+              autoComplete='off'
+            />
+          </li>
+          <li className='mb-6'>
+            <Input
+              value={login}
+              onChange={e => setLogin(e.target.value)}
+              placeholder={'Логин'}
+              type={'email'}
+              icon={'EditIcon'}
+              autoComplete='off'
+            />
+          </li>
+          <li className='mb-6'>
+            <Input
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              placeholder={'Пароль'}
+              type={'password'}
+              icon={'EditIcon'}
+              autoComplete='off'
+            />
+          </li>
+        </ul>
+
+        <div className={`mt-2 ${styles.bottom}`}>
+          <button className={`${styles.reject} text text_type_main-default text_color_inactive`}>Отмена</button>
+          <Button htmlType="submit" type="primary" size="medium">Сохранить</Button>
+        </div>
+      </form>
+    </>
+  )
+}
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,24 +1,40 @@
-import { Link } from 'react-router-dom';
+
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import styles from './profile.module.css';
-import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import ProfileForm from '../components/profile/profile-form';
 
 export function ProfilePage() {
+
+  const activeClasses = `text text_type_main-medium ${styles.link}`;
+  const inactiveClasses = `text_color_inactive text text_type_main-medium ${styles.link}`;
+
+
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.sidebar}>
           <ul className='mb-20'>
-            <li><Link to="/profile" className={`text text_type_main-medium ${styles.link}`}>Профиль</Link></li>
-            <li><Link to="/" className={`text_color_inactive text text_type_main-medium ${styles.link}`}>История Заказов</Link></li>
+            <li>
+              <NavLink
+                to="/profile" end
+                className={({ isActive }) => isActive ? activeClasses : inactiveClasses}>
+                Профиль
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/profile/orders"
+                className={({ isActive }) => isActive ? activeClasses : inactiveClasses}>
+                История Заказов</NavLink>
+            </li>
             <li><Link to="/" className={`text_color_inactive text text_type_main-medium ${styles.link}`}>Выход</Link></li>
           </ul>
           <p className='text_type_main-small text_color_inactive'>В этом разделе вы можете <br />
             изменить свои персональные данные</p>
         </div>
-        <div className={styles.inputs}>
-          <Input placeholder={'Имя'} type={'text'} icon={'EditIcon'} disabled />
-          <Input placeholder={'Логин'} type={'email'} icon={'EditIcon'} disabled />
-          <Input placeholder={'Пароль'} type={'password'} icon={'EditIcon'} disabled />
+        <div>
+          <ProfileForm />
+          <Outlet />
         </div>
       </div>
     </>
