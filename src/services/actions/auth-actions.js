3b844eb70c355a4eedb8fa156763096ec367fcd6
Grigoriy Gavrilov <greeb.spb@gmail.com>
2023-02-13 21:26:03 +0300

    add reducers, actions

--- src/services/actions/auth-actions.js
+++ src/services/actions/auth-actions.js
@@ -1,21 +1,140 @@
-import { loginRequest } from "../../utils/burger-api";
-import { LOGIN_USER, LOGIN_USER_REQUEST, LOGIN_USER_ERROR } from "../action-types";
 
-export const loginUser = (form) => (dispatch) => {
+import {
+  loginRequest,
+  registerRequest,
+  userRequest,
+  refreshTokenRequest,
+  saveTokens,
+  userChangeRequest,
+  logOutRequest
+} from "../../utils/burger-api";
+import {
+  LOGIN_USER,
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_ERROR,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_ERROR,
+  LOGOUT_USER_REQUEST,
+  LOGOUT_USER_SUCCESS,
+  LOGOUT_USER_ERROR
+} from "../action-types";
+
+export const loginUser = (form, cb) => (dispatch) => {
   dispatch({
     type: LOGIN_USER_REQUEST
   });
 
   return loginRequest(form).then((data) => {
-    if (data.ok) {
-      dispatch({
-        type: LOGIN_USER,
-        payload: data.user
-      })
+
+    if (data.accessToken) {
+      const authToken = data.accessToken;
+      saveTokens(data.refreshToken, authToken);
+    }
+
+    dispatch({
+      type: LOGIN_USER,
+      payload: data
+    })
+    cb();
+
+  }).catch(() => {
+    dispatch({
+      type: LOGIN_USER_ERROR
+    })
+  })
+}
+
+export const registerUser = (form, cb) => (dispatch) => {
+  dispatch({
+    type: REGISTER_USER_REQUEST
+  });
+
+  return registerRequest(form).then((data) => {
+
+    if (data.accessToken) {
+      const authToken = data.accessToken;
+      saveTokens(data.refreshToken, authToken);
+    }
+
+    dispatch({ type: REGISTER_USER_SUCCESS, payload: data });
+    cb();
+
+  }).catch(() => {
+    dispatch({
+      type: REGISTER_USER_ERROR
+    })
+  })
+}
+
+export const getUser = () => (dispatch) => {
+  dispatch({
+    type: GET_USER_REQUEST
+  });
+
+  return userRequest().then((data) => {
+    dispatch({
+      type: GET_USER_SUCCESS,
+      payload: data
+    })
+  }).catch((error) => {
+    if (error.message === 'jwt expired') {
+      dispatch(refreshToken(getUser()));
     } else {
       dispatch({
-        type: LOGIN_USER_ERROR
-      })
+        type: GET_USER_ERROR,
+        payload: error.message,
+      });
     }
   })
 }
+
+export const changeUser = (form) => (dispatch) => {
+  dispatch({
+    type: GET_USER_REQUEST
+  });
+
+  return userChangeRequest(form).then((data) => {
+    dispatch({
+      type: GET_USER_SUCCESS,
+      payload: data
+    })
+  }).catch((error) => {
+    if (error.message === 'jwt expired') {
+      dispatch(refreshToken(changeUser(form)));
+    } else {
+      dispatch({
+        type: GET_USER_ERROR,
+        payload: error.message,
+      });
+    }
+  })
+}
+
+export const logOut = (cb) => (dispatch) => {
+  dispatch({
+    type: LOGOUT_USER_REQUEST
+  });
+
+  return logOutRequest().then((data) => {
+    if (data.success) {
+      dispatch({
+        type: LOGOUT_USER_SUCCESS
+      });
+      cb();
+    }
+  }).catch(() => {
+    dispatch({ type: LOGOUT_USER_ERROR })
+  })
+}
+
+const refreshToken = (afterRefresh) => (dispatch) => {
+  refreshTokenRequest()
+    .then((res) => {
+      saveTokens(res.refreshToken, res.accessToken);
+      dispatch(afterRefresh);
+    })
+};
--- src/services/reducers/auth-reducer.js
+++ src/services/reducers/auth-reducer.js
@@ -1,6 +1,20 @@
-import { LOGIN_USER, LOGIN_USER_REQUEST, LOGIN_USER_ERROR } from "../action-types"
+import {
+  LOGIN_USER,
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_ERROR,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_ERROR,
+  LOGOUT_USER_REQUEST,
+  LOGOUT_USER_SUCCESS,
+  LOGOUT_USER_ERROR
+} from "../action-types";
 
 const initialState = {
+  isAuthenticated: false,
   isLoading: false,
   isError: false,
   user: {
@@ -11,6 +25,26 @@ const initialState = {
 
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_USER_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      }
+    case GET_USER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        isAuthenticated: true,
+        user: action.payload.user
+      }
+    case GET_USER_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      }
     case LOGIN_USER_REQUEST:
       return {
         ...state,
@@ -20,7 +54,10 @@ export const authReducer = (state = initialState, action) => {
     case LOGIN_USER:
       return {
         ...state,
-        user: action.payload
+        user: action.payload.user,
+        isAuthenticated: true,
+        isLoading: false,
+        isError: false
       }
     case LOGIN_USER_ERROR:
       return {
@@ -28,9 +65,49 @@ export const authReducer = (state = initialState, action) => {
         isLoading: false,
         isError: true
       }
+    case REGISTER_USER_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      }
+    case REGISTER_USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload.user,
+        isAuthenticated: true,
+        isLoading: false,
+        isError: false
+      }
+    case REGISTER_USER_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      }
+    case LOGOUT_USER_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      }
+    case LOGOUT_USER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        isAuthenticated: false,
+        user: null
+      }
+    case LOGOUT_USER_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      }
+
 
     default:
       return state
   }
 }
-
